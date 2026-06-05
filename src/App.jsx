9e6fc3db941ecba54e18d08e263ef55d56ac4106
Hilmi Arkan <maulanahelmy7@gmail.com>
2026-06-06 05:57:08 +0700

    feat(App.jsx): learned map(), JSX, JS object, React function
    BREAKING CHANGE:

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,79 @@
-const title = "React";
+import "./App.css";
+
+function getTitle(title) {
+  return title;
+}
+
+const welcome = {
+  title: "react",
+  greeting: "ahoy there",
+};
+
+function getNull() {
+  return null;
+}
+
+const list = ["laptop", "iphone xs", "macbook air", "claude"];
+const number = [1, 2, 3, 4];
+const exponentialNumber = number
+  .map(function (number) {
+    return number * number;
+  })
+  .join(", ");
+
+const books = [
+  {
+    title: "book of numbers",
+    author: "jesus",
+    num_comment: 5,
+    url: "https://bookofnumbers.com",
+    point: 7,
+    objectID: 0,
+  },
+  {
+    title: "quran",
+    author: "allah",
+    num_comment: 999999,
+    url: "https://quran.com",
+    point: 9999999,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hellooo {title}</h1>
-      <label htmlFor="input">
-        Type something
-        <input id="input" type="text" />
-      </label>
+      <div>
+        <h1>Hellooo {getTitle("react")}</h1>
+        <label htmlFor="input">
+          <p className="">{welcome.greeting}</p>
+          <input id="input" type="text" />
+        </label>
+      </div>
+      <div>
+        <h2>my things</h2>
+        <p>{list.map((x) => x).join(", ")}</p>
+      </div>
+      <div>
+        <h2>numbers</h2>
+        <p>{exponentialNumber}</p>
+      </div>
+      <div>
+        <h2>Books</h2>
+        {books.map(function (item) {
+          return (
+            <div key={item.objectID}>
+              <a href={item.url}>
+                <h3>{item.title}</h3>
+              </a>
+              <p>{item.author}</p>
+              <p>Comment: {item.num_comment}</p>
+              <p>{item.point} Point</p>
+            </div>
+          );
+        })}
+      </div>
+      {/* {commmennntt} */}
     </div>
   );
 }
