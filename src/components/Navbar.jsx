bdc95e3d762ff7cb22d41171db0b60d69c3a8913
Hilmi Arkan <maulanahelmy7@gmail.com>
2026-07-19 13:44:50 +0700

    feat: cleanup anything, add untitledUI and navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">wow</h1>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  // <h1>THIS SITE HAS BEEN SEIZED</h1>
   <StrictMode>
     <App />
   </StrictMode>,
